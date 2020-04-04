198bd829541e7ed73a2ddd18738b2aaeb2cdf947
missionLife <SSJ6Porfy@hotmail.com>
2020-04-04 17:53:07 -0400

    first deploy

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import AWS from 'aws-sdk';
+
+AWS.config.setPromisesDependency(Promise);
+AWS.config.update({ region: process.env.AWS_REGION });
+
+async function consume(event, context) {
+  console.log('GOT HERE!!!');
+  return {};
+}
+
+exports.handler = async (event, context) => {
+  try {
+    return await consume(event, context);
+  } catch (error) {
+    throw new Error(`An error occurred in the Mission Life Sponsorship API: ${error.message}`);
+  }
+};
