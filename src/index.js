ffa0e8d970baeb9c5c0f2958896b6f5ff14e717a
missionLife <SSJ6Porfy@hotmail.com>
2020-04-05 12:39:10 -0400

    adding cognito identity provider

--- src/index.js
+++ src/index.js
@@ -3,22 +3,44 @@ import AWS from 'aws-sdk';
 AWS.config.setPromisesDependency(Promise);
 AWS.config.update({ region: process.env.AWS_REGION });
 
-async function consume(event, context) {
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+
+async function getUserFromToken(event, context) {
   console.log('GOT HERE THE EVENT!!!', event);
 
-  return {
-    statusCode: 200,
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({ message: 'Success' })
+  try {
+    const user = await cognitoIdentityServiceProvider.getUser({
+      AccessToken: event.headers.Authorization
+    }).promise();
+  
+    console.log('########### THE USER!!!!!!', user);
+    
+    return {
+      statusCode: 200,
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({ message: 'Success' })
+    }  
+  } catch (error) {
+    throw new Error(error.message);
   }
 }
 
 exports.handler = async (event, context) => {
   try {
-    return await consume(event, context);
+    return await getUserFromToken(event, context);
   } catch (error) {
-    throw new Error(`An error occurred in the Mission Life Sponsorship API: ${error.message}`);
+    const messageObject = {
+      message: `An error occurred in the Mission Life Sponsorship API: ${error.message}`
+    };
+
+    return {
+      statusCode: 500,
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(messageObject)
+    } 
   }
 };
